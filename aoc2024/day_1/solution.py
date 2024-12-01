daccfd408b0077d4237401216ca2bf885e5649d2
aczelandi <aczelandi@gmail.com>
2024-12-01 18:08:46 +0100

    Day 1 solution

--- aoc2024/day_1/solution.py
+++ aoc2024/day_1/solution.py
@@ -1,2 +1,36 @@
-def solution() -> int:
-    return 0
+from heapq import heappop
+from pathlib import Path
+import re
+import heapq
+
+class Solution:
+
+    def solve_part1(self, input_file_path: Path) -> int:
+        (left, right) = self._read_input(input_file_path)
+
+        if len(left) != len(right):
+            raise ValueError("invalid input data provided")
+
+        heapq.heapify(left)
+        heapq.heapify(right)
+
+        return sum(abs(heappop(left) - heappop(right)) for _ in range(0, len(left)))
+
+    def solve_part2(self, input_file_path: Path) -> int:
+        (left, right) = self._read_input(input_file_path)
+        right_by_count = {number : right.count(number) for number in set(right)}
+
+        return sum(n * right_by_count.get(n, 0) for n in left)
+
+    @staticmethod
+    def _read_input(input_file_path: Path) -> (list[int], list[int]):
+        left = []
+        right = []
+        with open(input_file_path, 'r') as file:
+            for line in file:
+                (left_val, right_val) = re.split(r"\s+", line.strip("\n"))
+                left.append(int(left_val))
+                right.append(int(right_val))
+
+        return left, right
+
--- tests/day_1/test_solution.py
+++ tests/day_1/test_solution.py
@@ -1,5 +1,24 @@
-from aoc2024.day_1.solution import solution
+from pathlib import Path
+import pytest
 
-def test_solution():
-    result = solution()
-    assert result == 0
+from aoc2024.day_1.solution import Solution
+
+@pytest.mark.parametrize("input_file, expected", [
+    ("small_sample.txt", 11),
+    ("input_sample.txt", 2815556),
+])
+def test_solution_part1(input_file, expected):
+    input_file_path = Path(__file__).parent / input_file
+    solution = Solution()
+    result = solution.solve_part1(input_file_path = input_file_path)
+    assert result == expected
+
+@pytest.mark.parametrize("input_file, expected", [
+    ("small_sample.txt", 31),
+    ("input_sample.txt", 23927637),
+])
+def test_solution_part2(input_file, expected):
+    input_file_path = Path(__file__).parent / input_file
+    solution = Solution()
+    result = solution.solve_part2(input_file_path = input_file_path)
+    assert result == expected
